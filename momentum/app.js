b64c842c3a129ce7a22670a34c42969e2fc0b162
dlrmsgnl620 <dlrmsgnl620@naver.com>
2022-01-01 19:34:22 +0900

    chromeApp ~#2.4

--- momentum/app.js
+++ momentum/app.js
@@ -1,13 +1,3 @@
-const a = 5;
-const b = 2;
-let myName = "Geuni";
-
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-
-console.log("My name " + myName);
-
-myName = "LeeKeunHwee";
-
-console.log("Your new name is " + myName);
+const amIFat = null;
+let something;
+console.log(something);
