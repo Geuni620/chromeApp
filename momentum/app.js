b14e50380c07c43f7e695e99bb3561984f19865d
dlrmsgnl620 <dlrmsgnl620@naver.com>
2022-01-02 16:05:19 +0900

    chromeApp ~#2.5

--- momentum/app.js
+++ momentum/app.js
@@ -1,3 +1,8 @@
-const amIFat = null;
-let something;
-console.log(something);
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+// Get Item from Array
+console.log(daysOfWeek);
+
+// Add one more day to the array
+daysOfWeek.push("sun");
+console.log(daysOfWeek);
