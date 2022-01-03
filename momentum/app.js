73ed51895d2e9b43ad83c2a6acf85b990742cae7
dlrmsgnl620 <dlrmsgnl620@naver.com>
2022-01-03 16:47:47 +0900

    chromeApp ~#2.11

--- momentum/app.js
+++ momentum/app.js
@@ -1,23 +1,24 @@
-const calculater = {
+const calculator = {
   add: function (a, b) {
-    console.log(a + b);
+    return a + b;
   },
-  minus: function (a, b) {
-    console.log(a - b);
+  sub: function (a, b) {
+    return a - b;
   },
   muti: function (a, b) {
-    console.log(a * b);
+    return a * b;
   },
   divide: function (a, b) {
-    console.log(a / b);
+    return a / b;
   },
-  power: function (a, b) {
-    console.log(a ** b);
+  sud: function (a, b) {
+    return a ** b;
   },
 };
 
-calculater.add(10, 5);
-calculater.minus(10, 5);
-calculater.muti(10, 5);
-calculater.divide(10, 5);
-calculater.power(10, 5);
+//서로가 서로를 의존하고 있음(서로 연결되어 있음)
+const plusResult = calculator.add(2, 3);
+const subResult = calculator.sub(plusResult, 10);
+const mutiResult = calculator.muti(10, subResult);
+const divideResult = calculator.divide(mutiResult, plusResult);
+const sudResult = calculator.sud(divideResult, subResult);
