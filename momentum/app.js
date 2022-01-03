23ef41ab2efb2b08b515202fd19a54ed9be0fdb5
dlrmsgnl620 <dlrmsgnl620@naver.com>
2022-01-03 22:05:33 +0900

    chromeApp ~#2.13

--- momentum/app.js
+++ momentum/app.js
@@ -1,24 +1,3 @@
-const calculator = {
-  add: function (a, b) {
-    return a + b;
-  },
-  sub: function (a, b) {
-    return a - b;
-  },
-  muti: function (a, b) {
-    return a * b;
-  },
-  divide: function (a, b) {
-    return a / b;
-  },
-  sud: function (a, b) {
-    return a ** b;
-  },
-};
+const age = parseInt(prompt("How old are you?"));
 
-//서로가 서로를 의존하고 있음(서로 연결되어 있음)
-const plusResult = calculator.add(2, 3);
-const subResult = calculator.sub(plusResult, 10);
-const mutiResult = calculator.muti(10, subResult);
-const divideResult = calculator.divide(mutiResult, plusResult);
-const sudResult = calculator.sud(divideResult, subResult);
+console.log(age);
