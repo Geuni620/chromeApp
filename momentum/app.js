218779a161fcc3fcda360db5454b318c82d40ca8
dlrmsgnl620 <dlrmsgnl620@naver.com>
2022-01-06 22:44:57 +0900

    chromeApp ~#3.2

--- momentum/app.js
+++ momentum/app.js
@@ -1,6 +1,16 @@
-const title = document.getElementById("title");
+document.getElementById();
+document.getElementsByClassName();
+//array를 가져다줌
+document.getElementsByTagName();
+//array를 가져다줌
+document.querySelector();
+//CSS selector를 사용하여 검색할 수 있음
+//element를 가져다줌
+document.querySelectorAll();
+//조건에 맞는 모든 element를 찾고 싶다면 all()을 쓰기
 
-title.innerText = "Got you";
-
-console.log(title.id);
-console.log(title.className);
+//추가로 id를 찾고 싶을 때도 querySelector()을 쓸 수 있음
+// ex)
+// const title = document.querySelectorAll("#hello");
+// const title = document.getElementById("hello");
+// ## 두개는 같은 일을 한다고 보면 돼
