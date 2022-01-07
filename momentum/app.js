9301e8b5a56a973b7b37c04a84a2fc26b081f536
dlrmsgnl620 <dlrmsgnl620@naver.com>
2022-01-07 21:08:15 +0900

    chromeApp ~#3.4

--- momentum/app.js
+++ momentum/app.js
@@ -4,4 +4,14 @@ function handleTitleClick() {
   title.style.color = "blue";
 }
 
+function handleMouseEnter() {
+  title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+  title.innerText = "Mouse is gone!";
+}
+
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
