41afe19187c7834bac944c063673ab80c1a344b9
dlrmsgnl620 <dlrmsgnl620@naver.com>
2022-01-07 20:09:57 +0900

    chromeApp ~#3.3

--- momentum/app.js
+++ momentum/app.js
@@ -1,16 +1,7 @@
-document.getElementById();
-document.getElementsByClassName();
-//array를 가져다줌
-document.getElementsByTagName();
-//array를 가져다줌
-document.querySelector();
-//CSS selector를 사용하여 검색할 수 있음
-//element를 가져다줌
-document.querySelectorAll();
-//조건에 맞는 모든 element를 찾고 싶다면 all()을 쓰기
+const title = document.querySelector(".hello:first-child h1");
 
-//추가로 id를 찾고 싶을 때도 querySelector()을 쓸 수 있음
-// ex)
-// const title = document.querySelectorAll("#hello");
-// const title = document.getElementById("hello");
-// ## 두개는 같은 일을 한다고 보면 돼
+function handleTitleClick() {
+  title.style.color = "blue";
+}
+
+title.addEventListener("click", handleTitleClick);
