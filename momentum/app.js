b9358410281a1412bb4fb32898bba1856ed077b9
dlrmsgnl620 <dlrmsgnl620@naver.com>
2022-01-09 11:09:02 +0900

    chromeApp ~#3.7

--- momentum/app.js
+++ momentum/app.js
@@ -1,14 +1,12 @@
 const h1 = document.querySelector(".hello:first-child h1");
 
 function handleTitleClick() {
-  const currentColor = h1.style.color;
-  let newColor;
-  if (currentColor === "blue") {
-    newColor = "tomato";
+  const clickedClass = "clicked sexy-font";
+  if (h1.className === clickedClass) {
+    h1.className = "";
   } else {
-    newColor = "blue";
+    h1.className = clickedClass;
   }
-  h1.style.color = newColor;
 }
 
 h1.addEventListener("click", handleTitleClick);
