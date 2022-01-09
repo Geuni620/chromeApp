4d6fac083602cca4cdc9214993d9a8ef36f3a67d
dlrmsgnl620 <dlrmsgnl620@naver.com>
2022-01-09 14:46:16 +0900

    chromeApp ~#4.0

--- momentum/app.js
+++ momentum/app.js
@@ -1,7 +1,8 @@
-const h1 = document.querySelector(".hello:first-child h1");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
-function handleTitleClick() {
-  h1.classList.toggle("clicked");
+function onLoginBtnClick() {
+  console.log("hello", loginInput.value);
 }
 
-h1.addEventListener("click", handleTitleClick);
+loginButton.addEventListener("click", onLoginBtnClick);
