7faca0983a7767e0c84b03fb4e1e2841a9139a78
dlrmsgnl620 <dlrmsgnl620@naver.com>
2022-01-10 20:28:18 +0900

    chromeApp ~#4.2

--- momentum/app.js
+++ momentum/app.js
@@ -1,9 +1,9 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
-  const username = loginInput.value;
-  console.log(username);
+function onLoginSubmit(event) {
+  event.preventDefault();
+  console.log(loginInput.value);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
