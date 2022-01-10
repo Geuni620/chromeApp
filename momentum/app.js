7d02b6a2dbe1b3fd0b6c68d567bdf7994578f64d
dlrmsgnl620 <dlrmsgnl620@naver.com>
2022-01-10 13:52:45 +0900

    chromeApp Challenge day.1

--- momentum/app.js
+++ momentum/app.js
@@ -1,8 +1,1 @@
-const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
-
-function onLoginBtnClick() {
-  console.log("hello", loginInput.value);
-}
-
-loginButton.addEventListener("click", onLoginBtnClick);
+alert("Hi");
