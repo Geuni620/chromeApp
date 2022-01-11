8f351cdef4d43ab62bc42aa71ae98a9f02b73a1c
dlrmsgnl620 <dlrmsgnl620@naver.com>
2022-01-12 00:04:11 +0900

    chromeApp ~#4.4

--- momentum/app.js
+++ momentum/app.js
@@ -1,17 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  console.log(loginInput.value);
-}
-
-function handleLinkClick(eventOne) {
-  alert("clicked");
-  eventOne.preventDefault();
-  console.dir(eventOne);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  greeting.innerText = "Hello " + username;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
