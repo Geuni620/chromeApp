dfbae814405b9c04a3fe9ed3397fd5d4864f39cc
dlrmsgnl620 <dlrmsgnl620@naver.com>
2022-01-11 20:46:42 +0900

    Obb extraction

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,5 @@
-alert("Hi");
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+// “일주일에 홀 수 요일만 출력해줘”
+const daysOfWeekOdd = daysOfWeek.filter((length) => index % 2 === 0);
+console.log(daysOfWeekOdd);
