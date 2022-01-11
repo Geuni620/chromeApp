0359d91513248de1f3b019272131e7a395e26d24
dlrmsgnl620 <dlrmsgnl620@naver.com>
2022-01-11 22:44:22 +0900

    chromeApp ~#4.3

--- momentum/app.js
+++ momentum/app.js
@@ -1,9 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const link = document.querySelector("a");
 
 function onLoginSubmit(event) {
   event.preventDefault();
   console.log(loginInput.value);
 }
 
+function handleLinkClick(eventOne) {
+  alert("clicked");
+  eventOne.preventDefault();
+  console.dir(eventOne);
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
