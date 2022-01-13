2cf3c5af083be11424f774b7461bc81a99d9f54b
dlrmsgnl620 <dlrmsgnl620@naver.com>
2022-01-13 19:14:06 +0900

    chromeApp Challenge day.4

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,37 @@
-const age = parseInt(prompt("How old are you?"));
+const title = document.querySelector(".hello h1");
+
+function handleTitleClick() {
+  console.log("title is here!");
+}
+
+function handleMouseEnter() {
+  title.innerText = "Mouse is here";
+}
+
+function handleMouseLeave() {
+  title.innerText = "Mouse is gone";
+}
+
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+  alert("Copier");
+}
+
+function handleWindowOffline() {
+  alert("SOS no WIFI");
+}
+function handleWindowOnline() {
+  alert("ALL GOODDDD");
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
