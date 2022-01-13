9fe096bd2524ad760ed3b6b9d0269790d634786b
dlrmsgnl620 <dlrmsgnl620@naver.com>
2022-01-13 22:05:10 +0900

    chromeApp ~#4.6

--- momentum/app.js
+++ momentum/app.js
@@ -3,15 +3,30 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  greeting.innerText = "Hello " + username;
-  localStorage.setItem("username", username);
   greeting.innerText = `Hello ${username}`;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreeting(username);
+}
+
+function paintGreeting(username) {
+  greeting.innerText = `Hello ${saveUsername}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
+
+const saveUsername = localStorage.getItem(USERNAME_KEY);
+
+console.log(saveUsername);
+
+if (saveUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+} else {
+  paintGreeting(saveUsername);
+}
