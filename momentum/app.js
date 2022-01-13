8e28bb823772c8bd91072e01e8e7aa94d7a72f95
dlrmsgnl620 <dlrmsgnl620@naver.com>
2022-01-13 19:53:53 +0900

    chromeApp challenge assignment day.4

--- momentum/app.js
+++ momentum/app.js
@@ -1,37 +1,40 @@
-const title = document.querySelector(".hello h1");
+const colors = ["green", "blue", "red", "purple"];
 
-function handleTitleClick() {
-  console.log("title is here!");
-}
+const h1 = document.querySelector(".hello h1");
+console.dir(h1);
 
+//1. 마우스가 title위로 올라가면 텍스트가 변경.
 function handleMouseEnter() {
-  title.innerText = "Mouse is here";
+  h1.innerText = "The mouse is here";
+  h1.style.color = colors[0];
 }
 
+//2. 마우스 title을 벗어나면 텍스트가 변경.
 function handleMouseLeave() {
-  title.innerText = "Mouse is gone";
+  h1.innerText = "The mouse is gone";
+  h1.style.color = colors[1];
 }
 
+//3. 브라우저 창의 사이즈가 변하면 title이 변경
 function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
+  h1.innerText = "You juse resized!";
+  h1.style.color = colors[3];
 }
 
-function handleWindowCopy() {
-  alert("Copier");
+//4. 마우스를 우 클릭하면 title 변경
+function handleMouseCountextmenu() {
+  h1.innerText = "That was a right click";
+  h1.style.color = colors[2];
 }
 
-function handleWindowOffline() {
-  alert("SOS no WIFI");
-}
-function handleWindowOnline() {
-  alert("ALL GOODDDD");
-}
+//5. title의 색상은 colors 배열에 있는 색을 사용해야 함.
 
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
 
+window.addEventListener("contextmenu", handleMouseCountextmenu);
 window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+
+//6. .css .html 파일을 수정하지 말 것
+//7. 모든 함수 핸들러는 superEventHandler 내부에 작성.
+//8. 모든 조건이 충족되지 못하면 x
