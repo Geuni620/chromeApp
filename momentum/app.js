20fbcbe95c91659378533ed0d5b8ee541e2ca9a8
dlrmsgnl620 <dlrmsgnl620@naver.com>
2022-01-15 13:15:21 +0900

    chromeApp review #4.1

--- momentum/app.js
+++ momentum/app.js
@@ -2,8 +2,12 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function handleBtnClick() {
-  console.log("Hello, ", loginInput.value);
-  console.log("click");
+  const username = loginInput.value;
+  if (username === "") {
+    console.log("Please write your name");
+  } else if (username.length > 15) {
+    console.log("Your name is too long");
+  }
 }
 
 loginButton.addEventListener("click", handleBtnClick);
