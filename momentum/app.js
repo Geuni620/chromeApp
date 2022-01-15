edd45408c67d1dc83d20276b9221fd58b2cd4271
dlrmsgnl620 <dlrmsgnl620@naver.com>
2022-01-15 12:33:13 +0900

    chromeApp review #4.0

--- momentum/app.js
+++ momentum/app.js
@@ -1,18 +1,9 @@
-const body = document.querySelector("body");
-const colorArray = ["#2E8CD5", "#904FAD", "#EEBC12"];
-console.dir(body);
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
-function handleResize() {
-  const windowFrame = window.innerWidth;
-  body.innerText = "Hello";
-  body.style.color = "white";
-  if (windowFrame <= 500) {
-    body.style.background = colorArray[0];
-  } else if (windowFrame > 500 && windowFrame <= 1000) {
-    body.style.background = colorArray[1];
-  } else if (windowFrame > 1000) {
-    body.style.background = colorArray[2];
-  }
+function handleBtnClick() {
+  console.log("Hello, ", loginInput.value);
+  console.log("click");
 }
 
-window.addEventListener("resize", handleResize);
+loginButton.addEventListener("click", handleBtnClick);
