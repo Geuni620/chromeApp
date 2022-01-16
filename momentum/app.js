0a03da7f1cdf39048109a3b3e5ddaa45a010ace7
dlrmsgnl620 <dlrmsgnl620@naver.com>
2022-01-16 11:20:59 +0900

    chromeApp review #4.3

--- momentum/app.js
+++ momentum/app.js
@@ -1,9 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(tomato) {
   tomato.preventDefault();
   const username = loginInput.value;
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.dir(event);
+}
+
+link.addEventListener("click", handleLinkClick);
