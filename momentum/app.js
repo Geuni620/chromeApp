fd63d057d398c978283496d5a9e82ba85a5f8d31
dlrmsgnl620 <dlrmsgnl620@naver.com>
2022-01-17 15:23:29 +0900

    change of thinking, username dell

--- momentum/app.js
+++ momentum/app.js
@@ -8,7 +8,6 @@ const USERNAME_KEY = "username";
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
   paintGreetings();
 }
