7f9b7df2283e5d2447cbc72166bc279df7588347
dlrmsgnl620 <dlrmsgnl620@naver.com>
2022-01-17 15:17:07 +0900

    change of thinking

--- momentum/app.js
+++ momentum/app.js
@@ -10,11 +10,12 @@ function onLoginSubmit(event) {
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
-  paintGreetings(username);
+  paintGreetings();
 }
 
 //중복된 greeting를 function으로 묶음
-function paintGreetings(username) {
+function paintGreetings() {
+  const username = localStorage.getItem(USERNAME_KEY);
   greeting.innerText = `Hello + ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
@@ -26,5 +27,5 @@ if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-  paintGreetings(savedUsername);
+  paintGreetings();
 }
