f21f2b9f0875133617eef14d1fe89f76c6ba743d
dlrmsgnl620 <dlrmsgnl620@naver.com>
2022-01-22 15:03:37 +0900

    Todo list dell function

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -2,11 +2,23 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 function paintTodo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newTodo;
+
+  const button = document.createElement("button");
+  button.innerText = "X";
+
+  button.addEventListener("click", deleteToDo);
+
+  li.appendChild(span);
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
 
