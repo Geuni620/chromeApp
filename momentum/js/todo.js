549ac5e16305f2c90347166f53bdd02d7a30d748
dlrmsgnl620 <dlrmsgnl620@naver.com>
2022-01-22 16:48:32 +0900

    JSON ERROR TEST

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -2,6 +2,14 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, toDos);
+}
+
 function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
@@ -26,7 +34,21 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+  toDos.push(newTodo);
   paintTodo(newTodo);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+function sayHello() {
+  console.log("hello");
+}
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+console.log(savedToDos);
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  console.log(parsedToDos);
+}
