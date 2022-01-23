62390636969cbc4e9e43158e166bde31e25d6e7f
dlrmsgnl620 <dlrmsgnl620@naver.com>
2022-01-23 19:38:45 +0900

    navigator bar 온도 추가

--- top-project/js/weather.js
+++ top-project/js/weather.js
@@ -5,7 +5,7 @@ function onGeoOk(position) {
   const log = position.coords.longitude;
   console.log("You live in", lat, log);
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}&units=metric`;
   console.log(url);
   fetch(url)
     .then((response) => response.json())
@@ -14,7 +14,7 @@ function onGeoOk(position) {
       const weather = document.querySelector(".weather span");
 
       city.innerText = data.name;
-      weather.innerText = data.weather[0].main;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}℃`;
     });
 }
 
